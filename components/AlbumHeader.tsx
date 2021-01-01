96a566a807a1a72d9b52a2f18b6d16eb9c5280ff
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-01 17:50:46 -0600

    render out song list in album screen

---
+++ components/AlbumHeader.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+export const AlbumHeader: FC = ({}) => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+};
--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,22 +1,73 @@
+import { useRoute } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 import styled from 'styled-components/native';
 import { SongItem } from '../components/SongItem';
 import { TSong } from '../types';
 const DefaultImage = require('../assets/images/album-cover-1.jpg');
 
-const song: TSong = {
-  id: '1',
-  imageUri: DefaultImage,
-  artist: 'Taylor Swift',
-  title: 'Willow',
+const songs: TSong[] = [
+  {
+    id: '1',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+  {
+    id: '2',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+  {
+    id: '3',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+  {
+    id: '4',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+  {
+    id: '5',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+  {
+    id: '6',
+    imageUri: DefaultImage,
+    artist: 'Taylor Swift',
+    title: 'Willow',
+  },
+];
+
+type TRoute = {
+  key: string;
+  name: string;
+  params: {
+    id: string;
+  };
 };
 
 export const AlbumScreen = () => {
+  const {
+    params: { id },
+  } = useRoute<TRoute>();
+  console.log(id);
   return (
     <SContainer>
-      <SongItem song={song} />
+      <FlatList
+        data={songs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return <SongItem song={item} />;
+        }}
+      />
     </SContainer>
   );
 };
