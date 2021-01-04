23ad051c1abf392b64336d9fb04940264f001eed
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-03 18:03:30 -0600

    add graphql query to get album categories

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components/native';
 import { AlbumCategory } from '../components/AlbumCategory';
 import albumData from '../assets/data/albumCategories';
 import { FlatList } from 'react-native';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listAlbumCategorys } from '../graphql/queries';
 
 export default function HomeScreen() {
+  useEffect(() => {
+    const fetchAlbumCategories = async () => {
+      try {
+        const data = await API.graphql(graphqlOperation(listAlbumCategorys));
+        console.log(data);
+      } catch {}
+    };
+  }, []);
+
   return (
     <SHomeContainer>
       <FlatList
