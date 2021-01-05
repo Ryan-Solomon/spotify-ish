2195a17abebcbee33b887c559088c70777f3c446
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-05 13:29:02 -0600

    add song search input

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -4,13 +4,26 @@ import styled from 'styled-components/native';
 export default function TabTwoScreen() {
   return (
     <SContainer>
-      <SText>Search Screen</SText>
+      <STextInput placeholder='Search for a song' />
     </SContainer>
   );
 }
 
-const SContainer = styled.View``;
+const SContainer = styled.View`
+  align-items: center;
+  padding: 3px;
+`;
 
 const SText = styled.Text`
   color: white;
+`;
+
+const STextInput = styled.TextInput`
+  color: #222;
+  height: 50px;
+  width: 90%;
+  background-color: white;
+  font-size: 22px;
+  padding: 8px;
+  border-radius: 5px;
 `;
