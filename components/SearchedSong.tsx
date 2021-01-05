cb84d974616e8516ccbea0bd60780da5246e1f20
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-05 17:04:01 -0600

    add image to searched song

--- components/SearchedSong.tsx
+++ components/SearchedSong.tsx
@@ -1,18 +1,30 @@
 import React, { FC } from 'react';
 import styled from 'styled-components/native';
 import { TSearchedSong } from '../screens/SearchScreen';
+import { Image, StyleSheet } from 'react-native';
 
 type TProps = {
   song: TSearchedSong;
 };
 
-export const SearchedSong: FC<TProps> = ({ song: { strTrack } }) => {
+export const SearchedSong: FC<TProps> = ({
+  song: { strTrack, strTrackThumb },
+}) => {
   return (
     <SContainer>
       <SText>{strTrack}</SText>
+      <Image style={styles.image} source={{ uri: strTrackThumb }} />
     </SContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: 'cover',
+    height: 50,
+    width: 50,
+  },
+});
 
 const SContainer = styled.View``;
 
