47c9aceab9365a43253d6c44b70b472ac375d145
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-05 19:34:54 -0600

    add song details component

--- components/SearchedSongDetails.tsx
+++ components/SearchedSongDetails.tsx
@@ -19,20 +19,39 @@ export const SearchedSongDetails: FC<TProps> = ({ song }) => {
   } = song;
   return (
     <SContainer>
-      <SText>Album: {strAlbum}</SText>
-      <SText>Track: {strTrack}</SText>
-      <SText>Artist: {strArtist}</SText>
-      <SText>Genre: {strGenre}</SText>
-      <SText>Mood: {strMood}</SText>
-      <SText>Plays: {intTotalPlays}</SText>
+      <SDetailsContainer>
+        <SText fontSize='22px'>Song Details</SText>
+        <SText fontColor='#d5d3d3'>Album: {strAlbum}</SText>
+        <SText fontColor='#d5d3d3'>Track: {strTrack}</SText>
+        <SText fontColor='#d5d3d3'>Artist: {strArtist}</SText>
+        <SText fontColor='#d5d3d3'>Genre: {strGenre}</SText>
+        <SText fontColor='#d5d3d3'>Mood: {strMood}</SText>
+        <SText fontColor='#d5d3d3'>Plays: {intTotalPlays}</SText>
+      </SDetailsContainer>
+      <SDescContainer>
+        <SText fontSize='22px'>Description</SText>
+        <SText fontColor='#d5d3d3'>{strDescriptionEN}</SText>
+      </SDescContainer>
     </SContainer>
   );
 };
 
 const SContainer = styled.View`
   flex: 1;
+  justify-content: space-evenly;
+  padding: 30px;
 `;
 
-const SText = styled.Text`
-  color: white;
+type TextProps = {
+  fontSize: string;
+  fontColor: string;
+};
+
+const SText = styled.Text<Partial<TextProps>>`
+  color: ${({ fontColor }) => fontColor || 'white'};
+  font-size: ${({ fontSize }) => fontSize || '14px'};
+  margin-bottom: 3px;
 `;
+
+const SDetailsContainer = styled.View``;
+const SDescContainer = styled.View``;
