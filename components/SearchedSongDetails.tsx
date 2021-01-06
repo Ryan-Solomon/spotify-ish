f8f9e7583aa1a4b1c3fef552116a4372764c180c
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-05 18:12:54 -0600

    show initial details

--- components/SearchedSongDetails.tsx
+++ components/SearchedSongDetails.tsx
@@ -7,9 +7,31 @@ type TProps = {
   song: TSearchedSong;
 };
 
-export const SearchedSongDetails: FC<TProps> = ({}) => {
-  return <SText>Searched Song Details</SText>;
+export const SearchedSongDetails: FC<TProps> = ({ song }) => {
+  const {
+    strAlbum,
+    strGenre,
+    strTrack,
+    strMood,
+    strDescriptionEN,
+    intTotalPlays,
+    strArtist,
+  } = song;
+  return (
+    <SContainer>
+      <SText>Album: {strAlbum}</SText>
+      <SText>Track: {strTrack}</SText>
+      <SText>Artist: {strArtist}</SText>
+      <SText>Genre: {strGenre}</SText>
+      <SText>Mood: {strMood}</SText>
+      <SText>Plays: {intTotalPlays}</SText>
+    </SContainer>
+  );
 };
+
+const SContainer = styled.View`
+  flex: 1;
+`;
 
 const SText = styled.Text`
   color: white;
